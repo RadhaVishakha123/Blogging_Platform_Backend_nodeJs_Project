1aaee278a27bc784d4162f37aa12ac17fac86a21
RadhaVishakha123 <vjangir@techturbine.com>
2025-12-05 13:09:58 +0530

    home page all public post display , and in profile page user post display

--- Blogging_Backend/src/controllers/postController.js
+++ Blogging_Backend/src/controllers/postController.js
@@ -1,4 +1,6 @@
 const Post=require("..//models/post");
+const UserProfile=require("..//models/userProfile")
+const Default_User="..//assets/Default_User.jpg";
 async function handleAddPost(req,res) {
     try {
     const { userId, content } = req.body;
@@ -22,5 +24,60 @@ async function handleAddPost(req,res) {
     res.status(500).json({ message: "Server Error" });
   }
 }
-module.exports={handleAddPost}
+async function fetchAllPostwithUserProfileData(req,res) {
+   try {
+    // 1. All posts from DB
+    const posts = await Post.find().sort({ createdAt: -1 }).lean();
+
+    // 2. Extract all userIds
+    const userIds = posts.map(post => post.userId);
+
+    // 3. Fetch profiles for all userIds
+    const profiles = await UserProfile.find({
+      userId: { $in: userIds }
+    }).lean();
+
+    // 4. Merge post + userProfile
+    const mergedPosts = posts.map(post => {
+      const user = profiles.find(
+        profile => profile.userId.toString() === post.userId.toString()
+      );
+
+      return {
+        ...post,
+        fullName: user?.fullName || "Unknown User",
+        profilePic: user?.profilePic ||null,
+        accountType: user?.accountType || "public",
+      };
+    });
+
+    res.status(200).json({
+      message: "Success",
+      data: mergedPosts
+    });
+
+  } catch (err) {
+    console.log("Error in getAllPosts", err);
+    res.status(500).json({ message: "Server Error", error: err.message });
+  }
+}
+async function fetchUserAllPost(req,res) {
+  const currentUserId=req.query.userId;
+  const userProfileData=await UserProfile.findOne({userId:{$eq:currentUserId}}).lean();
+  const posts=await Post.find({userId:{$eq:currentUserId}}).lean()
+  const postData=posts.map((post)=>{
+    return{
+      ...post,
+      fullName: userProfileData?.fullName || "Unknown User",
+      profilePic:userProfileData?.profilePic ||null,
+      accountType: userProfileData?.accountType || "public",
+    }
+  })
+  res.status(200).json({
+    message: "Success",
+    data:postData
+  })
+
+}
+module.exports={handleAddPost,fetchAllPostwithUserProfileData,fetchUserAllPost}
 
