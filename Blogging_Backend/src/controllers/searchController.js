333e4bce7236fe01481686570bb0c46313f29790
RadhaVishakha123 <vjangir@techturbine.com>
2025-12-08 15:22:29 +0530

    update search controller

--- Blogging_Backend/src/controllers/searchController.js
+++ Blogging_Backend/src/controllers/searchController.js
@@ -1,7 +1,8 @@
 const UserProfile=require("..//models/userProfile");
 const User=require("..//models/auth");
 async function searchUser(req,res) {
-try{const userProfile=await UserProfile.find({});
+try{const query = req.query.query;
+    const userProfile=await UserProfile.find({});
 const user=await User.find({});
 const mergedUsers= user.map((user)=>{
     const profile= userProfile.find((p)=>p.userId.toString()==user._id.toString() )
@@ -14,7 +15,15 @@ const mergedUsers= user.map((user)=>{
     accountType: profile?.accountType || "public",
     }
 })
- return res.status(200).json({ mergedUsers });
+if(query){
+    const filtered=mergedUsers.filter((u) =>
+  u.username.toLowerCase().includes(query) ||
+  u.fullName.toLowerCase().includes(query)
+)
+return res.status(200).json({ filtered });
+
+}
+ return res.status(200).json({  });
 }
 catch (error) {
     console.log(error);
