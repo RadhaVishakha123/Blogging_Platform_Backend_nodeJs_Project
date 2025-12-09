9d0c27380261b7be71faef20b958f3aaf602f25b
RadhaVishakha123 <vjangir@techturbine.com>
2025-12-09 16:08:26 +0530

    update function of count follower & following

--- Blogging_Backend/src/routes/followRoute.js
+++ Blogging_Backend/src/routes/followRoute.js
@@ -1,11 +1,10 @@
 const express=require("express")
 const router=express.Router();
-const {handleUnFollow, handleFollow, checkIsFollowing,countFollowing,countFollower,fetchUserFollower,fetchUserFollowing }=require("../controllers/followController")
+const {handleUnFollow, handleFollow, checkIsFollowing,countFollowingFollower,fetchUserFollower,fetchUserFollowing }=require("../controllers/followController")
 router.post("/unfollowuser",handleUnFollow)
 router.post("/followuser",handleFollow)
 router.get("/",checkIsFollowing)
-router.get("/countFollowing",countFollowing)
-router.get("/countFollower",countFollower)
+router.get("/count",countFollowingFollower)
 router.get("/fetchUserFollower",fetchUserFollower)
 router.get("/fetchUserFollowing",fetchUserFollowing)
 module.exports = router;
